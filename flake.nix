24b216acff02f879177a006d15d4952b54f52f70
zenneh <wertelaerssenne@icloud.com>
2024-11-28 14:54:50 +0100

    add docker to flake.nix

--- flake.nix
+++ flake.nix
@@ -1,26 +1,42 @@
 {
-  description = "The ubermench nodeJS development flake";
+  description = "The ubermench nodeJS development flake with Docker";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    darwin.url = "github:lnl7/nix-darwin";
+    darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs = {
     self,
     nixpkgs,
     flake-utils,
+    darwin,
   }:
-    flake-utils.lib.eachDefaultSystem (
-      system: let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            nodejs_22
-            nodePackages.yarn
-            nodePackages.typescript-language-server
-            nodePackages.vscode-langservers-extracted
-          ];
-        };
-      }
-    );
+    {
+      darwinConfigurations.docker = darwin.lib.darwinSystem {
+        system = "aarch64-darwin"; # Change if using Intel Mac
+        modules = [
+          ({pkgs, ...}: {
+            virtualisation.docker.enable = true;
+            services.nix-daemon.enable = true;
+          })
+        ];
+      };
+    }
+    // flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          nodejs_22
+          nodePackages.yarn
+          nodePackages.typescript-language-server
+          nodePackages.vscode-langservers-extracted
+
+          docker
+          docker-compose
+          docker-client
+        ];
+      };
+    });
 }

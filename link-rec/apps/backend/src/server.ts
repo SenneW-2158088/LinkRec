cc6eb4fe1920dfee926f44f8eaa9d6b26d70a6ff
zenneh <wertelaerssenne@icloud.com>
2024-11-29 01:38:28 +0100

    fix this mess

--- link-rec/apps/backend/src/server.ts
+++ link-rec/apps/backend/src/server.ts
@@ -2,12 +2,14 @@ import http from 'http';
 
 console.log('hello friend');
 
+const port = process.env.PORT || 3030;
+
 // Create a simple HTTP server that keeps the process alive
 const server = http.createServer((req, res) => {
     res.writeHead(200);
     res.end('Server is running');
 });
 
-server.listen(3000, () => {
+server.listen(port, () => {
     console.log('Server is listening on port 3000');
 });

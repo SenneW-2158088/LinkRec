fb09ed1eb65b6b5f695335ffc5e41ad20fb968c9
zenneh <wertelaerssenne@icloud.com>
2024-11-30 14:49:10 +0100

    fix server port

--- link-rec/apps/backend/src/server.ts
+++ link-rec/apps/backend/src/server.ts
@@ -3,14 +3,17 @@ import { GraphQLID, GraphQLObjectType, GraphQLSchema, GraphQLString } from "grap
 import { startStandaloneServer } from '@apollo/server/standalone';
 import { UserField } from "./types/user";
 import { linkRecSchema } from "./schema";
+import { env } from "process";
 
 const server = new ApolloServer({
   schema: linkRecSchema
 })
 
+const PORT = Number(process.env.BACKEND_PORT)
+
 async function startServer() {
   const { url } = await startStandaloneServer(server, {
-    listen: { port: 4000 }
+    listen: { port: PORT }
   })
 
   console.log(`🚀  Server ready at: ${url}`);
